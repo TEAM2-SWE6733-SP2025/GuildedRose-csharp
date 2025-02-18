d153d44e7cbe00952d56cb8eed88fbb954b0008d
Kirsten Huttar <Kirstenmt@outlook.com>
2025-02-17 19:13:46 -0500

    inital commit
    initial commit for gilded rose activity in csharp

---
+++ csharp.xUnit/GildedRoseTests/GildedRoseTest.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+using System.Collections.Generic;
+using GildedRoseKata;
+
+namespace GildedRoseTests;
+
+public class GildedRoseTest
+{
+    [Fact]
+    public void DefaultQualityDecrease()
+    {
+        IList<Item> Items = new List<Item> { new Item { Name = "Elixir of the Mongoose", SellIn = 0, Quality = 1 } };
+        GildedRose app = new GildedRose(Items);
+        app.UpdateQuality();
+        Assert.Equal(0, Items[0].Quality);
+    }
+
+    //TODO make the rest of the unit tests
+}
